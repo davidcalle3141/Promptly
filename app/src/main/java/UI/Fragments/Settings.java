361d49b61ccfbb70a7853abc0ef570c0d1d58866
David Calle <davidcalle3141@gmail.com>
2018-10-05 16:17:39 -0400

    connected text size to the seek bar need

--- app/src/main/java/UI/Fragments/Settings.java
+++ app/src/main/java/UI/Fragments/Settings.java
@@ -3,31 +3,60 @@ package UI.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import calle.david.promptly.R;
 
 
 public class Settings extends Fragment {
+    View mView;
 
+    @BindView(R.id.settings_seek_bar)SeekBar mSeekBar;
+    @BindView(R.id.settings_text_view)TextView mTextView;
 
     public Settings() {
         // Required empty public constructor
     }
 
-
+//TODO save to preferance font size
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        mView= inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this,mView);
+        return mView;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mTextView.setTextSize((float) i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
 
 }
