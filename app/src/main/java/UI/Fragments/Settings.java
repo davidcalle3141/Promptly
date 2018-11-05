d19fa614a14614925d5458b7b649b3f86e9ac115
David Calle <davidcalle3141@gmail.com>
2018-11-04 21:28:36 -0500

    settings scroll created attached to teleprompter

--- app/src/main/java/UI/Fragments/Settings.java
+++ app/src/main/java/UI/Fragments/Settings.java
@@ -1,17 +1,19 @@
 package UI.Fragments;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -24,9 +26,14 @@ public class Settings extends Fragment {
     View mView;
     Context mContext;
     Activity mActivity;
-
+    SharedPreferences sharedPreferences;
+    @BindView(R.id.settings_scroll_view)
+    ScrollView mScrollView;
+    @BindView(R.id.settings_seek_bar_speed)
+    SeekBar mSeekBarScrollSpeed;
     @BindView(R.id.settings_seek_bar)SeekBar mSeekBar;
     @BindView(R.id.settings_text_view)TextView mTextView;
+    private ObjectAnimator mAnimator;
 
     public Settings() {
         // Required empty public constructor
@@ -39,41 +46,186 @@ public class Settings extends Fragment {
         mContext = getContext();
         mActivity = getActivity();
         ButterKnife.bind(this,mView);
+        String text = getResources().getString(R.string.settings_text);
+        mTextView.setText(text);
         return mView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        sharedPreferences = mActivity.getPreferences(Context.MODE_PRIVATE);
+
+        mScrollView.post(this::populateUI);
+
+
+    }
+
+    private void populateUI() {
+
         int currentSize = (int) mTextView.getTextSize();
-        float defaultValue = currentSize / getResources().getDisplayMetrics().scaledDensity;
-        SharedPreferences sharedPreferences = mActivity.getPreferences(Context.MODE_PRIVATE);
-        float sp = sharedPreferences.getFloat("PROMPTER_TEXT_SIZE",defaultValue);
+        float defaultValueSize = currentSize / getResources().getDisplayMetrics().scaledDensity;
+        float sp = sharedPreferences.getFloat("PROMPTER_TEXT_SIZE", defaultValueSize);
+
+
         mTextView.setTextSize(sp);
 
-        mSeekBar.setProgress((int) (sp));
+        ObjectAnimator textSizeAnimator =
+                ObjectAnimator.ofInt(mSeekBar, "progress", (int) sp - 30);
+        textSizeAnimator.setInterpolator(new LinearInterpolator());
+        textSizeAnimator.setDuration(1000);
+        textSizeAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                handleText();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        int defaultValueScrollSpeed = 5;
+        int scrollSpeed = sharedPreferences.getInt("PROMPTER_SCROLL_SPEED", defaultValueScrollSpeed);
+
+        ObjectAnimator scrollSpeedAnimator =
+                ObjectAnimator.ofInt(mSeekBarScrollSpeed, "progress", scrollSpeed);
+        scrollSpeedAnimator.setInterpolator(new LinearInterpolator());
+        scrollSpeedAnimator.setDuration(1000);
+        scrollSpeedAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                handleScroll();
+                startScrollAnimation(scrollSpeed);
+                startScroll();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        textSizeAnimator.start();
+        scrollSpeedAnimator.start();
+
+
+    }
+
+
+    private void startScrollAnimation(int scrollSpeed) {
+
+        mAnimator = ObjectAnimator.ofInt(mScrollView, "scrollY", mTextView.getBottom());
+        mAnimator.setDuration(mTextView.getLineCount() * (scrollSpeed * 100));
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        mAnimator.addPauseListener(new Animator.AnimatorPauseListener() {
+            @Override
+            public void onAnimationPause(Animator animator) {
+                startScrollAnimation(scrollSpeed);
+            }
+
+            @Override
+            public void onAnimationResume(Animator animator) {
+
+            }
+        });
+
+
+    }
+
+    private void handleScroll() {
+        mSeekBarScrollSpeed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                startScrollAnimation(i + 1);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                sharedPreferences.edit().putInt("PROMPTER_SCROLL_SPEED", seekBar.getProgress() + 1).apply();
+                startScroll();
+            }
+        });
+    }
+
+    public void handleText() {
+
+
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if(i<15) i=15;
+                i = i + 30;
                 mTextView.setTextSize((float) i);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                mAnimator.end();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 int progress = seekBar.getProgress();
-                if(progress<15) progress = 15;
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putFloat("PROMPTER_TEXT_SIZE", progress);
+                startScroll();
+                editor.putFloat("PROMPTER_TEXT_SIZE", progress + 30);
                 editor.apply();
 
             }
         });
+
+    }
+
+    private void startScroll() {
+        if (mAnimator.isPaused()) mAnimator.resume();
+        else if (!mAnimator.isRunning()) mAnimator.start();
+
+
     }
 
 }
--- app/src/main/java/UI/Fragments/TelePrompt.java
+++ app/src/main/java/UI/Fragments/TelePrompt.java
@@ -1,6 +1,8 @@
 package UI.Fragments;
 
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -8,31 +10,40 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
-import java.util.LinkedList;
-
+import Utils.AppExecutors;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import calle.david.promptly.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TelePrompt extends Fragment {
 
-    View mView;
-    Context mContext;
-    Activity mActivity;
-    String text=null;
-    LinkedList<String> hotWordLL;
+    @BindView(R.id.tele_prompt_text_view)
+    TextView mTextView;
+    @BindView(R.id.tele_prompt_scroll_view)
+    ScrollView mScrollview;
 
-    float sp;
-
-    @BindView(R.id.tele_prompt_text_view)TextView mTextView;
+    private float sp;
+    private View mView;
+    private Context mContext;
+    private Activity mActivity;
+    private String text = null;
+    private int SCREEN_SCROLL_STATUS = 0;
+    private AppExecutors mExecutors;
+    private ObjectAnimator animator;
+    private SharedPreferences sharedPreferences;
 
 
     public TelePrompt() {
@@ -46,9 +57,7 @@ public class TelePrompt extends Fragment {
         mView = inflater.inflate(R.layout.fragment_tele_prompt, container, false);
         mContext = getContext();
         mActivity = getActivity();
-        ButterKnife.bind(this,mView);
-
-        // Inflate the layout for this fragment
+        ButterKnife.bind(this, mView);
         return mView;
 
 
@@ -58,28 +67,83 @@ public class TelePrompt extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         text = mActivity.getResources().getString(R.string.test_tele_string);
-        hotWordLL = new LinkedList<>();
-        SharedPreferences sharedPreferences = mActivity.getPreferences(Context.MODE_PRIVATE);
-        sp = sharedPreferences.getFloat("PROMPTER_TEXT_SIZE",50);
+        sharedPreferences = mActivity.getPreferences(Context.MODE_PRIVATE);
+        sp = sharedPreferences.getFloat("PROMPTER_TEXT_SIZE", 50);
         mTextView.setTextSize(sp);
         mTextView.setText(text);
-
-        mTextView.post(() -> splitIntoLines());
-
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
+        mTextView.post(this::ScrollControl);
+        mExecutors = AppExecutors.getInstance();
 
 
     }
 
-    private void splitIntoLines() {
-        int numOfLines = mTextView.getLayout().getLineCount();
-        String tempString = null;
-        String tempStringAry[];
-        for(int i = 0; i<numOfLines;i++){
-        tempString = text.substring(mTextView.getLayout().getLineStart(i),mTextView.getLayout().getLineEnd(i));
-        tempString = tempString.trim();
-        tempStringAry = tempString.split("\\s+");
-        hotWordLL.add(tempStringAry[tempStringAry.length-1]);
+    private void ScrollControl() {
+        if (animator != null) animator.cancel();
+
+
+        animator = ObjectAnimator.ofInt(mScrollview, "scrollY", mTextView.getBottom());
+        animator.setDuration(mTextView.getLineCount() * sharedPreferences.getInt("PROMPTER_SCROLL_SPEED", 5) * 100);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addListener(new Animator.AnimatorListener() {
+
+
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+            }
+
+        });
+        animator.addPauseListener(new Animator.AnimatorPauseListener() {
+
+            @Override
+            public void onAnimationPause(Animator animator) {
+                ScrollControl();
+            }
+
+            @Override
+            public void onAnimationResume(Animator animator) {
+
+            }
+        });
+
+
+    }
+
+    @OnClick(R.id.tele_prompt_text_view)
+    public void clickScreen() {
+        if (SCREEN_SCROLL_STATUS == 0) {
+            SCREEN_SCROLL_STATUS = 1;
+            startScroll();
+        } else if (SCREEN_SCROLL_STATUS == 1) {
+            SCREEN_SCROLL_STATUS = 0;
+            pauseScroll();
         }
+    }
+
+    private void pauseScroll() {
+        animator.pause();
+    }
+
+    private void startScroll() {
+        if (animator.isPaused()) animator.resume();
+        else if (!animator.isRunning()) animator.start();
+
 
     }
 
